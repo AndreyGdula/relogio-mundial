ce301a4b17a617105fb02217d0eb53937d9aeb27
AndreyGdula <andreygdula@gmail.com>
2024-02-14 22:31:38 -0300

    fullscreen finalizada

--- script/script.js
+++ script/script.js
@@ -110,14 +110,16 @@ atualizaHora()
 
 function fullscreen() {
     var body = document.getElementById("body")
+    var horariosSections = document.querySelectorAll(".horarios");
+    var localMargin = document.querySelector("main")
+    var icon = document.getElementById("fullscreen-icon");
     if (!document.fullscreenElement) {
         body.requestFullscreen().then(() => {
             document.body.style.backgroundColor = "black";
-            var horariosSections = document.querySelectorAll(".horarios");
-            var localMargin = document.querySelector("p#local")
             horariosSections.forEach(section => {
                 localMargin.style.margin = "200px 0px 0px 0px"
                 section.style.display = "none";
+                icon.innerText = 'close_fullscreen'
             });
         }).catch((err) => {
             console.log(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
@@ -125,9 +127,10 @@ function fullscreen() {
     } else {
         document.exitFullscreen().then(() => {
             document.body.style.backgroundColor = "";
-            var horariosSections = document.querySelectorAll(".horarios");
             horariosSections.forEach(section => {
+                localMargin.style.margin = ""
                 section.style.display = "flex";
+                icon.innerText = 'open_in_full'
             });
         }).catch((err) => {
             console.log(`Error attempting to exit full-screen mode: ${err.message} (${err.name})`);
