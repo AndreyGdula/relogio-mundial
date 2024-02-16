57d1e4e19cf4d0eab0b6375ece69be377ba30c07
AndreyGdula <andreygdula@gmail.com>
2024-02-15 22:05:47 -0300

    fix bug
    bug fullscreen

--- script/londres-script.js
+++ script/londres-script.js
@@ -95,34 +95,3 @@ function atualizaHoraMundo() {
 setInterval(atualizaHora, 100)
 atualizaHora()
 atualizaHoraMundo()
-
-
-function fullscreen() {
-    var body = document.getElementById("body")
-    var horariosSections = document.querySelectorAll(".horarios");
-    var localMargin = document.querySelector("main")
-    var icon = document.getElementById("fullscreen-icon");
-    if (!document.fullscreenElement) {
-        body.requestFullscreen().then(() => {
-            document.body.style.backgroundColor = "black";
-            horariosSections.forEach(section => {
-                localMargin.style.margin = "200px 0px 0px 0px"
-                section.style.display = "none";
-                icon.innerText = 'close_fullscreen'
-            });
-        }).catch((err) => {
-            console.log(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-        });
-    } else {
-        document.exitFullscreen().then(() => {
-            document.body.style.backgroundColor = "";
-            horariosSections.forEach(section => {
-                localMargin.style.margin = ""
-                section.style.display = "flex";
-                icon.innerText = 'open_in_full'
-            });
-        }).catch((err) => {
-            console.log(`Error attempting to exit full-screen mode: ${err.message} (${err.name})`);
-        });
-    }
-}
--- script/script.js
+++ script/script.js
@@ -106,34 +106,3 @@ setInterval(atualizaHora, 100)
 
 // Inicializa a hora com o horário atual
 atualizaHora()
-
-
-function fullscreen() {
-    var body = document.getElementById("body")
-    var horariosSections = document.querySelectorAll(".horarios");
-    var localMargin = document.querySelector("main")
-    var icon = document.getElementById("fullscreen-icon");
-    if (!document.fullscreenElement) {
-        body.requestFullscreen().then(() => {
-            document.body.style.backgroundColor = "black";
-            horariosSections.forEach(section => {
-                localMargin.style.margin = "200px 0px 0px 0px"
-                section.style.display = "none";
-                icon.innerText = 'close_fullscreen'
-            });
-        }).catch((err) => {
-            console.log(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-        });
-    } else {
-        document.exitFullscreen().then(() => {
-            document.body.style.backgroundColor = "";
-            horariosSections.forEach(section => {
-                localMargin.style.margin = ""
-                section.style.display = "flex";
-                icon.innerText = 'open_in_full'
-            });
-        }).catch((err) => {
-            console.log(`Error attempting to exit full-screen mode: ${err.message} (${err.name})`);
-        });
-    }
-}
